700e32e678202e013ae30c67aca088fc65f7386a
Lukasz Kowalski <write@lukaszkowalski.pro>
2013-11-02 14:15:25 +0100

    Initial Commit

---
+++ trunk/Wise.Framework/Wise.Framework.Interfaces/DataAccessLayer/IRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Wise.Framework.Interfaces.DataAccessLayer
+{
+    public interface IRepository: IDisposable
+    {
+        void Save(object obj);
+    }
+}
