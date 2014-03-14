7c2c6d8981f26c96a4451392c156e34f21dc75c9
Lukasz Kowalski <kowalski@nieprzecietny.pl>
2014-03-14 22:28:33 +0100

    missing files part 2

---
+++ Wise.Framework/Wise.Framework.Environment/EnvironmentInfo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Wise.Framework.Interface.Environment;
+
+namespace Wise.Framework.Environment
+{
+    public class EnvironmentInfo :IEnvironmentInfo
+    {
+        public bool SelfContained
+        { get; set;
+        }
+
+        public string Code
+        {
+            get;
+            set;
+        }
+
+        public string Address
+        {
+            get;
+            set;
+        }
+
+        public string HostName
+        {
+            get;
+            set;
+        }
+    }
+}
---
+++ Wise.Framework/Wise.Framework.Environment/EnvironmentService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Wise.Framework.Interface.Environment;
+
+namespace Wise.Framework.Environment
+{
+    public class EnvironmentService : IEnvironmentService
+    {
+        readonly EnvironmentSection config = (EnvironmentSection)ConfigurationManager.GetSection("environmentSection/environment");
+        public IEnvironmentInfo GetEnvironmentInfo()
+        {
+            return new EnvironmentInfo()
+            {
+                SelfContained = config.SelfContained,
+                Code = config.SelfContained ? System.Environment.MachineName : config.Server.EnvCode,
+                HostName = config.SelfContained ? System.Environment.MachineName : config.Server.Hostname,
+                Address = config.SelfContained ? "127.0.0.1" : config.Server.Address,
+            };
+        }
+    }
+}
