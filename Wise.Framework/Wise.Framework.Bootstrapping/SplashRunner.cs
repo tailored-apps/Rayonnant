6063acb3d91c829680553a97d74d876b2c6a2b8b
nieprzecietny <kowalski@nieprzecietny.pl>
2014-12-16 19:39:30 +0100

    push

--- Wise.Framework/Wise.Framework.Bootstrapping/SplashRunner.cs
+++ Wise.Framework/Wise.Framework.Bootstrapping/SplashRunner.cs
@@ -40,7 +40,6 @@ namespace Wise.Framework.Bootstrapping
 
         public void CloseSplash()
         {
-            //this need to be checked
             Dispatcher.FromThread(splashThread).Invoke(splash.Close);
             viewModel.Dispose();
         }
