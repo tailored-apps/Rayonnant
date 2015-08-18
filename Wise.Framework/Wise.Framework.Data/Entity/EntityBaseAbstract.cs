9937adc27cad1d23f6a2b9b223983d2b414d795f
Kowalski <Kowalski@DELL.wise.corp>
2015-08-18 21:52:48 +0200

    WF-2: two key model

--- Wise.Framework/Wise.Framework.Data/Entity/EntityBaseAbstract.cs
+++ Wise.Framework/Wise.Framework.Data/Entity/EntityBaseAbstract.cs
@@ -9,7 +9,7 @@
         {
             if (obj is TEntity)
             {
-                var oth = (EntityBaseAbstract<TKey, TEntity>) obj;
+                var oth = (EntityBaseAbstract<TKey, TEntity>)obj;
                 return Id.Equals(oth.Id);
             }
             return false;
@@ -26,7 +26,37 @@
 
         public override string ToString()
         {
-            return string.Format("#{0}#{1}", typeof (TEntity), Id);
+            return string.Format("#{0}#{1}", typeof(TEntity), Id);
+        }
+    }
+
+    public abstract class EntityBaseAbstract<TKey, TkeyTwo, TEntity> : EntityBaseAbstract<TKey, TEntity>
+    {
+        public virtual TkeyTwo IdTwo { get; protected set; }
+
+
+        public override bool Equals(object obj)
+        {
+            if (obj is TEntity)
+            {
+                var oth = (EntityBaseAbstract<TKey, TkeyTwo, TEntity>)obj;
+                return Id.Equals(oth.Id) && IdTwo.Equals(oth.IdTwo);
+            }
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            if (!Id.Equals(default(TKey))&& !IdTwo.Equals(default(TkeyTwo)))
+            {
+                return ToString().GetHashCode();
+            }
+            return base.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return string.Format("#{0}#{1}:{2}", typeof(TEntity), Id, IdTwo);
         }
     }
 }
