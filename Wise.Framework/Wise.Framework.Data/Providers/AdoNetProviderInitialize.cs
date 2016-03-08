e970bac5c9ac0f896402088220467d03999e3e9d
Lukasz Kowalski <kowalski@nieprzecietny.pl>
2016-03-09 00:17:32 +0100

    ado net client

---
+++ Wise.Framework/Wise.Framework.Data/Providers/AdoNetProviderInitialize.cs
@@ -0,0 +1,35 @@
+﻿
+using System.Collections.Generic;
+using Wise.Framework.Data.Enum;
+using Wise.Framework.Interface.DependencyInjection;
+
+namespace Wise.Framework.Data.Providers
+{
+    public abstract class AdoNetProviderInitialize : IInitialize
+    {
+        protected readonly IContainer container;
+
+        public AdoNetProviderInitialize(IContainer container)
+        {
+            this.container = container;
+        }
+
+        public void Initialize()
+        {
+            var conf = new AdoNetDbPersister();
+
+            BuildMappings(conf);
+            PostProcessConfiguration(conf);
+
+            container.RegisterInstance(conf);
+        }
+
+
+
+        protected abstract void BuildMappings(AdoNetDbPersister conf);
+
+        protected virtual void PostProcessConfiguration(AdoNetDbPersister cfg)
+        {
+        }
+    }
+}
